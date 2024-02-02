ce479d1160e5668aafc173ee084672e7828f7911
ramezHamdiSa'eed <317170274@sha.edu.eg>
2024-02-02 23:32:50 +0200

    view pager 2

---
+++ app/src/main/java/com/example/foody/screens/ViewPagerActivity.java
@@ -0,0 +1,71 @@
+package com.example.foody.screens;
+
+import android.os.Bundle;
+
+import com.example.foody.R;
+import com.example.foody.ui.viewPager.VPAdapter;
+import com.example.foody.ui.viewPager.ViewPagerItem;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+import androidx.viewpager2.widget.ViewPager2;
+
+
+import java.util.ArrayList;
+
+public class ViewPagerActivity extends AppCompatActivity {
+
+    ViewPager2 viewPager2;
+    ArrayList<ViewPagerItem> viewPagerItemArrayList;
+    Button nextbtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pager);
+        viewPager2 = findViewById(R.id.viewPager);
+        nextbtn=findViewById(R.id.nextbtn);
+
+        int[] images = {R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e};
+        String[] heading = {"Baked","Grilled","Dessert","Italian","Shakes"};
+        String[] desc = {getString(R.string.a_desc),
+                getString(R.string.b_desc),
+                getString(R.string.c_desc),
+                getString(R.string.d_desc)
+                ,getString(R.string.e_desc)};
+
+        viewPagerItemArrayList = new ArrayList<>();
+
+        for (int i =0; i< images.length ; i++){
+
+            ViewPagerItem viewPagerItem = new ViewPagerItem(images[i],heading[i],desc[i]);
+            viewPagerItemArrayList.add(viewPagerItem);
+
+        }
+
+        VPAdapter vpAdapter = new VPAdapter(viewPagerItemArrayList);
+
+        viewPager2.setAdapter(vpAdapter);
+
+        viewPager2.setClipToPadding(false);
+
+        viewPager2.setClipChildren(false);
+
+        viewPager2.setOffscreenPageLimit(2);
+
+        viewPager2.getChildAt(0).setOverScrollMode(View.OVER_SCROLL_NEVER);
+        nextbtn.setOnClickListener(v -> Toast.makeText(ViewPagerActivity.this, "clicked", Toast.LENGTH_SHORT).show());
+
+
+
+    }
+
+
+}
